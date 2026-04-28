85b769d418fec83b0ad108ea2786db313b6c8e73
Ivan <durin.s_son@mail.ru>
2026-04-28 13:14:43 +0300

    Add project files.

---
+++ Domain/AuthCode.cs
@@ -0,0 +1,18 @@
+﻿namespace Domain
+{
+    public class AuthCode
+    {
+        public Guid Id { get; set; }
+
+        public string Code { get; set; } = string.Empty;
+
+        public DateTime ExperiTime { get; set; }
+
+        public bool IsUsed { get; set; }
+
+
+        public Guid UserId { get; set; }
+
+        public User? User { get; set; }
+    }
+}
---
+++ Domain/User.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+
+        public string? Email { get; set; }
+
+        public long? TelegramId { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+
+        public List<AuthCode> Codes { get; set; } = new();
+    }
+}
