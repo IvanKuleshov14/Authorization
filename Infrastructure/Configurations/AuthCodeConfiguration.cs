759e759d479d9078544dbcf0cdd80d59f8abe181
Ivan <durin.s_son@mail.ru>
2026-05-07 19:30:04 +0300

    Добавлен DbContext и настроены конфигурации для User и AuthCode

---
+++ Infrastructure/Configurations/AuthCodeConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class AuthCodeConfiguration : IEntityTypeConfiguration<AuthCode>
+{
+    public void Configure(EntityTypeBuilder<AuthCode> builder)
+    {
+        builder.
+            HasKey(c => c.Id);
+
+        builder.
+            Property(c => c.Code).
+            IsRequired().
+            HasMaxLength(10);
+
+        builder.
+            Property(c => c.ExpiryTime).
+            IsRequired();
+
+        builder.
+            Property(c => c.IsUsed).
+            HasDefaultValue(false);
+    }
+}
---
+++ Infrastructure/Data/AuthorizationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Domain;
+using Infrastructure.Configurations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+
+namespace Infrastructure.Data
+{
+    public class AuthorizationDbContext : DbContext
+    {
+        public AuthorizationDbContext(DbContextOptions options) : base(options) {}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+            modelBuilder.ApplyConfiguration(new AuthCodeConfiguration());
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<AuthCode> AuthCodes { get; set; }
+    }
+}
