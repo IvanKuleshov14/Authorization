9fb23d07f44c2ffc4e9886a821a1c42960198a97
Ivan <durin.s_son@mail.ru>
2026-05-18 20:24:34 +0300

    Реализованы методы добавления, получения и обновления кода в AuthCodesRepository

--- Application/AuthCode/Interfaces/IAuthCodesRepository.cs
+++ Application/AuthCode/Interfaces/IAuthCodesRepository.cs
@@ -5,5 +5,7 @@ namespace Application.AuthCode.Interfaces
     public interface IAuthCodesRepository
     {
         Task AddAsync(Domain.AuthCode authCode);
+        Task<Domain.AuthCode?> GetLastCodeByUderIdAsync(Guid userId);
+        Task UpdateAsync(Domain.AuthCode authCode);
     }
 }
--- Infrastructure/Repositories/AuthCodesRepository.cs
+++ Infrastructure/Repositories/AuthCodesRepository.cs
@@ -1,13 +1,35 @@
 ﻿using Application.AuthCode.Interfaces;
 using Domain;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata.Ecma335;
 
 namespace Infrastructure.Repositories
 {
     public class AuthCodesRepository : IAuthCodesRepository
     {
+        private readonly AuthorizationDbContext _dbContext;
+        public AuthCodesRepository(AuthorizationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
         public async Task AddAsync(AuthCode authCode)
         {
-            throw new NotImplementedException();
+            await _dbContext.AuthCodes.AddAsync(authCode);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<AuthCode?> GetLastCodeByUderIdAsync(Guid userId)
+        {
+            return await _dbContext.AuthCodes.Where(c => c.UserId == userId && c.IsUsed == false)
+                .OrderByDescending(c => c.ExpiryTime)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task UpdateAsync(AuthCode authCode)
+        {
+            _dbContext.AuthCodes.Update(authCode);
+            await _dbContext.SaveChangesAsync();
         }
     }
 }
