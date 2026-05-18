556a4733e625d811730096f34e0ddeb340e7a4b2
Ivan <durin.s_son@mail.ru>
2026-05-18 19:11:42 +0300

    Реализованы методы AddAsync и GetByIdentityAsync в UsersRepository

--- Infrastructure/Repositories/UsersRepository.cs
+++ Infrastructure/Repositories/UsersRepository.cs
@@ -1,18 +1,32 @@
 ﻿using Application.Users.Interfaces;
 using Domain;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata.Ecma335;
 
 namespace Infrastructure.Repositories
 {
     public class UsersRepository : IUsersRepository
     {
+        private readonly AuthorizationDbContext _dbContext;
+        public UsersRepository(AuthorizationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
         public async Task AddAsync(User user)
         {
-            throw new NotImplementedException();
+            await _dbContext.Users.AddAsync(user);
+            await _dbContext.SaveChangesAsync();
         }
 
         public async Task<User?> GetByIdentityAsync(string identity)
         {
-            throw new NotImplementedException();
+            long.TryParse(identity, out long tgId);
+
+            return await _dbContext.Users.FirstOrDefaultAsync(
+                u => (tgId != 0 && u.TelegramId == tgId) || u.Email == identity
+                );
         }
     }
 }
